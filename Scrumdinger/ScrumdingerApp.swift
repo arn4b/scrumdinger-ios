31ab678fe5c2bd23d13bd9fb57eab243faa01afb
Arnab Chatterjee <arnabc1701@gmail.com>
2022-09-20 23:30:24 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Arnab Chatterjee on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
